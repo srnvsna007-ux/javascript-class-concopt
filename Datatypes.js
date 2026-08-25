2e45c33a70e6831802e175471b2ea27500a65af7
Sankar Narayanan <srnvsna007@gmail.com>
2026-08-25 21:37:15 +0530

    Create Datatypes.js with data types explanation
    Added a new file Datatypes.js that explains data types in JavaScript, including examples of primitive and non-primitive types.

---
+++ Datatypes.js
@@ -0,0 +1,24 @@
+/* DATA TYPES 
+1. Primitive data type 
+2. Non primitive data type 
+
+1.PRIMITIVE DATA TYPE 
+Number, string , boolean, null , undefined, symbol,bigint ;
+2.NON PRIMITIVE DATA TYPE 
+objects, arrays and function */
+//example for primitive data type 
+ let Number="";
+let String ="";
+let Boolean="";
+let data;// undefined 
+let Symbol=Symbol ();
+let bigint=122n;
+Console.log("1"+Number+"2"+String+"3"+Boolean+"4"+bigint);
+// example for non primitive data type 
+let person {name:"",age:};// objects 
+let  array =["",""];
+function name ()
+{
+}// function declaration 
+ const name =()=>{
+ }// arrow declaration 
